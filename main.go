e545e8cd6b137cc7ae44021fb35d622ff5d32839
achmadardian <achmadardian37@gmail.com>
2025-04-17 13:28:33 +0700

    chore: add gin-gionic/gin

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	//
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "ok")
+	})
+
+	log.Printf("server running at http://localhost:8080 port")
+	r.Run()
 }
