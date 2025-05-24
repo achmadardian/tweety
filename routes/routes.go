f3057e7a47943e620ef02beaf71e35bb425926a2
achmadardian <achmadardian37@gmail.com>
2025-05-24 11:51:37 +0700

    feat: remove all user related code

--- routes/routes.go
+++ routes/routes.go
@@ -3,8 +3,6 @@ package routes
 import (
 	"votes/config"
 	"votes/handlers"
-	"votes/repositories"
-	"votes/services"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,24 +10,11 @@ import (
 func InitRoutes(r *gin.Engine, DB *config.Database) {
 	// injection
 	healthcheckHandler := handlers.NewHealthcheck()
-	userRepo := repositories.NewUserRepository(DB)
-	userService := services.NewUserService(userRepo)
-	userHandler := handlers.NewUserHandler(userRepo, userService)
 
 	// routes
 	api := r.Group("api")
 	{
 		// healthcheck
 		api.GET("/", healthcheckHandler.GetHealth)
-
-		// user
-		users := api.Group("/users")
-		{
-			users.GET("/", userHandler.GetUserAll)
-			users.GET("/:id", userHandler.GetUserById)
-			users.POST("/", userHandler.CreateUser)
-			users.PATCH("/:id", userHandler.UpdateUser)
-			users.DELETE("/:id", userHandler.DeleteUser)
-		}
 	}
 }
