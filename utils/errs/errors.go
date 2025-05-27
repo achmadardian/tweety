e4ac5e8e9d8013f21c1c59c87a9473aecb5875bb
achmadardian <achmadardian37@gmail.com>
2025-05-27 15:46:24 +0700

    feat(errs): add new error

--- utils/errs/errors.go
+++ utils/errs/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrDataNotFound         = errors.New("data not found")
 	ErrEmailAlreadyExist    = errors.New("email already exist")
 	ErrUsernameAlreadyExist = errors.New("username already exist")
+	ErrInvalidLogin         = errors.New("invalid login")
 )
