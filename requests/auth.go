1955679adff979a9d7ef1d22f2b5fa10e67fa982
achmadardian <achmadardian37@gmail.com>
2025-05-27 12:43:10 +0700

    feat: remove validation req for lastname

--- requests/auth.go
+++ requests/auth.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type RegisterRequest struct {
 	ID        uuid.UUID `json:"-"`
 	FirstName string    `json:"first_name" binding:"required,max=50"`
-	LastName  string    `json:"last_name" binding:"required,max=50"`
+	LastName  string    `json:"last_name" binding:"max=50"`
 	Username  string    `json:"username" binding:"required,max=20"`
 	Email     string    `json:"email" binding:"required,email,max=254"`
 	Password  string    `json:"password" binding:"required,min=8"`
