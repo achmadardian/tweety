da7dd340f91cc896f9660c0e383c3b534e571504
achmadardian <achmadardian37@gmail.com>
2025-06-24 10:47:17 +0700

    feat(response): add role relation in Me response

--- responses/user.go
+++ responses/user.go
@@ -3,9 +3,10 @@ package responses
 import "github.com/google/uuid"
 
 type Me struct {
-	Id        uuid.UUID `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  *string   `json:"last_name"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
+	Id        uuid.UUID        `json:"id"`
+	FirstName string           `json:"first_name"`
+	LastName  *string          `json:"last_name"`
+	Username  string           `json:"username"`
+	Email     string           `json:"email"`
+	Role      RoleUserResponse `json:"role"`
 }
