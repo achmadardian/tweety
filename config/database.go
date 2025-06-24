b699491a21971624267b112044167db2a2799d63
achmadardian <achmadardian37@gmail.com>
2025-06-24 10:51:16 +0700

    feat(config): add seed for superadmin

--- config/database.go
+++ config/database.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"votes/models"
 	z "votes/utils/logger"
 
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -42,5 +46,57 @@ func InitDB() *Database {
 	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
+	// Seed superadmin if not exists
+	exists, err := checkSuperadmin(db)
+	if err != nil {
+		z.Log.Fatal().
+			Str("event", "database.check_superadmin").
+			Err(err).
+			Msg("failed to check if superadmin exists")
+	}
+
+	if !exists {
+		if err := seedSuperadmin(db); err != nil {
+			z.Log.Fatal().
+				Str("event", "database.seed_super_admin").
+				Err(err).
+				Msg("failed to seed superadmin")
+		}
+	}
+
 	return &Database{DB: db}
+}
+
+func checkSuperadmin(db *gorm.DB) (bool, error) {
+	var count int64
+	if err := db.Model(&models.User{}).Where("role_id = ?", 1).Count(&count).Error; err != nil {
+		return false, fmt.Errorf("check superadmin existence: %w", err)
+	}
+
+	return count > 0, nil
+}
+
+func seedSuperadmin(db *gorm.DB) error {
+	password := os.Getenv("SUPERADMIN_PASSWORD")
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("hashing password: %w", err)
+	}
+
+	lastName := "admin"
+	superadmin := models.User{
+		ID:        uuid.New(),
+		FirstName: "super",
+		LastName:  &lastName,
+		Username:  "superadmin",
+		Email:     "superadmin@example.com",
+		Password:  string(hashedPass),
+		RoleID:    1,
+	}
+
+	if err := db.Create(&superadmin).Error; err != nil {
+		return fmt.Errorf("seeding superadmin: %w", err)
+	}
+
+	return nil
 }
